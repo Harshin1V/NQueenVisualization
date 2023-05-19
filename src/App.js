8fb9c677e790f6dfafeec0c2a66325df00b9f5de
harshini1v <harshinivutukuri1@gmail.com>
2023-05-19 18:04:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NQueenVisualization from "./NQueenVisualization";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NQueenVisualization />
+    </div>
+  );
+}
+
+export default App;
